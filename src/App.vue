ef1d970a83cc0dcb01c6975c6bc19347ffb616b3
Darina67 <75675517+Darina67@users.noreply.github.com>
2023-07-27 21:02:26 +0300

    pagination_pages

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,121 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
-    <post-list :posts="posts" />
+    <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..."> </my-input>
+    <div class="app__btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOPtions"></my-select>
+    </div>
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+    <div class="page__wrapper">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        class="page"
+        :class="{ 'current-page': page === pageNumber }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import PostList from "@/components/PostList";
 import PostForm from "@/components/PostForm";
+import MyButton from "./components/UI/MyButton.vue";
+
+import axios from "axios";
 
 export default {
   components: {
     PostList,
     PostForm,
+    MyButton,
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: "Javascript", body: "Описание поста" },
-        { id: 2, title: "Javascript 2", body: "Описание поста 2" },
-        { id: 3, title: "Javascript 3", body: "Описание поста 3" },
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: "",
+      searchQuery: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOPtions: [
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержимому" },
       ],
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    changePage(pageNumber) {
+      this.page = pageNumber;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        setTimeout(async () => {
+          const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts",
+            {
+              params: {
+                _page: this.page,
+                _limit: this.limit,
+              },
+            }
+          );
+          this.totalPages = Math.ceil(
+            response.headers["x-total-count"] / this.limit
+          );
+          this.posts = response.data;
+          this.isPostsLoading = false;
+        }, 1000);
+      } catch (e) {
+        alert("ошибка");
+      } finally {
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+  },
+  watch: {
+    page() {
+      this.fetchPosts();
     },
   },
 };
@@ -39,5 +129,21 @@ export default {
 }
 .app {
   padding: 20px;
+}
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid teal;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h3>Список пользователей</h3>
-    <post-item v-for="post in posts" :post="post"></post-item>
+  <div v-show="posts.length > 0">
+    <h3>Список постов</h3>
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      ></post-item>
+    </transition-group>
   </div>
+  <h2 v-show="posts.length === 0" style="color: red">Список постов пуст</h2>
 </template>
 <script>
 import PostItem from "@/components/PostItem";
@@ -16,4 +24,17 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
